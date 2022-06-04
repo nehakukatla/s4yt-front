0bbe5d622377ada178e676be4b6cc28986dcae62
Neragin <mathirajanniranjan@gmail.com>
2022-06-04 12:47:06 -0700

    feat(form): added validation, css doesn't quite work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/login" />
-        <Route path="/register" />
-        <Route path="/forgot-password" />
-        <Route path="/game" />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/game" element={<Game />} />
+      <Route path="/login" element={<Login />} / >
+      <Route path="/register" element={<Register />} / >
+      <Route path="/forgot-password" element={<ForgotPassword />} / >
+    </Routes>
   );
+}
+
+
+
+function ForgotPassword() {
+  return <h2>Forgot Password</h2>;
+}
+
+function Game() {
+  return <h2>Game</h2>;
 }
 
 export default App;
