c5d10d2368c3d87e1f6525ab21b79af24d842177
Neragin <mathirajanniranjan@gmail.com>
2022-08-11 15:00:57 -0700

    added email url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import EmailVerification from "./EmailVerification";
+import EmailVerify from "./components/EmailVerify";
 import Register from "./Register";
 import "bootstrap/dist/css/bootstrap.css";
 import "./css/reset.css";
@@ -9,10 +11,12 @@ import "./css/s4yt.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-			<Route path="register" element={<Register />} />
-		</Routes>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="register" element={<Register />} />
+      <Route exact path="/email-verify" element={<EmailVerification />} />
+      <Route exact path="/email-verify/:id" element={<EmailVerify />} />
+    </Routes>
+  </BrowserRouter>
 );
